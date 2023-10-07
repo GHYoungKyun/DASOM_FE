3c56ee3c30d97fe8ac57fee2e2163ab0f5dcdfa7
GHYoungKyun <dudrbsapdlf@naver.com>
2023-10-07 17:50:22 +0900

    page routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes, Link } from 'react-router-dom';
+import Login from './Login';
+import MeetingRequest from './MeetingRequest';
+import MyPage from './MyPage';
+import Signup from './Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Routes>
+          <Route path="/login" element={<Login />} />                       
+          <Route path="/meetingreq" element={<MeetingRequest />} />         
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
     </div>
   );
 }
