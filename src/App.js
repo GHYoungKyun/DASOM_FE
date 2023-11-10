c9e90fd3ff78ac54799cb41a0c492f126cf0b5d1
GHYoungKyun <dudrbsapdlf@naver.com>
2023-11-11 00:14:48 +0900

    dev test

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
             <Route path="/write" element={<Write />} />
             <Route path="/read" element={<Read />} />
         </Routes>
+        <p>HOME PAGE</p>
     </div>
   );
 }
